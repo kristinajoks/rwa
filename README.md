cb00268b7650c43b5a0a87e1f77d2e0a9936a321
Kristina Joksimovic <joksimovic.kristina@elfak.rs>
2023-09-19 14:59:35 +0200

    trial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rwa
+RxjSnake 
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
